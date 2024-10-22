ab9453ab7a1b7eaf74ad109b88505d3a13584dd6
Lena_Bondar <lencha.2005@gmail.com>
2024-10-22 21:39:46 +0300

    task 1

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,30 @@
 import { Button } from '../Button/Button';
+// 1. Імпортуємо хук
+import { useSelector, useDispatch } from "react-redux";
+// 2. Імпортуємо фабрику екшену
+import { setStatusFilter } from "../../redux/actions";
+
 import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  // 3. Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+  // 2. Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(state => state.filters.status);
+
+  // 4. Викликаємо фабрику екшену та передаємо значення фільтра
+  // 5. Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      {/* <Button>All {filter === "all" && "is active"}</Button>
+      <Button>Active {filter === "active" && "is active"}</Button>
+      <Button>Completed {filter === "completed" && "is active"}</Button> */}
+      <Button onClick={() => handleFilterChange("all")} selected={filter === 'all'}>All</Button>
+      <Button onClick={() => handleFilterChange("active")} selected={filter === 'active'}>Active</Button>
+      <Button onClick={() => handleFilterChange("completed")} selected={filter === 'completed'}>Completed</Button>
     </div>
   );
 };
--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,28 @@
 import { Button } from '../Button/Button';
+// 1. Імпортуємо хук
+import { useDispatch } from "react-redux";
+// 2. Імпортуємо фабрику екшену
+import { addTask } from "../../redux/actions";
+
 import css from './TaskForm.module.css';
 
 export const TaskForm = () => {
+
+   // 3. Отримуємо посилання на функцію відправки екшенів
+   const dispatch = useDispatch();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+
+    // 4. Викликаємо фабрику екшену та передаємо дані для payload
+    // 5. Відправляємо результат - екшен створення завдання
+    dispatch(addTask({
+	    id: crypto.randomUUID(),  //вбудований у бпаузер метод 
+	    completed: false,
+	    text: form.elements.text.value
+	  }));
+
     form.reset();
   };
 
